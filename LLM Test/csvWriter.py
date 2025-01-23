1125d2f6af1c7be08c37f1299fa21cd60f74fbd9
Steffen Hauck <24982394+Stev-eH@users.noreply.github.com>
2025-01-23 10:12:48 +0100

    Newest version

---
+++ LLM Test/csvWriter.py
@@ -0,0 +1,39 @@
+import json
+
+
+class DataFile:
+    filePath = ""
+    data=[]
+
+
+    def __init__(self, filePath):
+        self.filePath = filePath
+        try:
+            self.data = self.read()
+        except FileNotFoundError:
+            self.write()
+
+
+    def append(self, model, parameters, elapsedTime, prompt, answer):
+        self.data.append({
+            "model": model,
+            "params": parameters,
+            "elapsed time": elapsedTime,
+            "prompt": prompt,
+            "answer": answer
+        })
+
+
+    def write(self):
+        with open(self.filePath, 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=2)
+
+
+    def read(self):
+        with open(self.filePath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+
+    def clear(self):
+        self.data = []
+        self.write()
--- LLM Test/main.py
+++ LLM Test/main.py
@@ -1,36 +1,64 @@
 import os
+
+import csvWriter
+
 os.environ['HF_HOME'] = 'C:\\LLM'
+
 
 import timeit
 
+
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login
+hfFile = open("C:\\LLM\\.cache\\huggingface\\token")
+login(token=hfFile.readline())
 
+# set values
+maxlength = 100
+
+
+#adjustable variables
 #enables the model to run on GPU
 device = "cuda"
+transformerLink = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
 
-tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B-0724-hf")
-model = AutoModelForCausalLM.from_pretrained("allenai/OLMo-1B-0724-hf").to(device)
+def loadModel(modelCode, device):
+    _tokenizer = AutoTokenizer.from_pretrained(modelCode)
+    _model = AutoModelForCausalLM.from_pretrained(modelCode).to(device)
+    return _tokenizer, _model
 
+#def loadModel(modelCode):
+#    _model = AutoModelForCausalLM.from_pretrained(modelCode, trust_remote_code=True)
+#    return _model
 
+tokenizer, model = loadModel(transformerLink, device)
 
-prompt = 0
+filepath = "C:\\LLM\\evaluation\\evaluations.json"
+datablocks = csvWriter.DataFile(filepath)
 
-while(prompt != "end"):
-    maxlength = int(input("maxlength: \n>>"))
-    # TO-DO: Automate prompts
-    prompt = input("prompt: \n>>")
-    if(prompt == "end"):
-        break
+prompts = ["Explain the stock market in as much detail as possible.",
+           "Wer war Martin Luther?",
+           "Which OS do Nokia Smartphones use?",
+           "If a planes crashes in US and lands on the Canada-US border, where do you bury the survivors?",
+           "Envision a plot for a blockbuster movie."]
+
+for prompt in prompts:
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
 # Generate
     tic = timeit.default_timer()
     generate_ids = model.generate(inputs.input_ids, max_length=maxlength)
     toc = timeit.default_timer()
-    print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0][len(prompt):])
+    response = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0][len(prompt):]
     # TO-DO: Automate saving responses and response times
     print("Generationtime with params maxlength (" + str(maxlength) + ") and prompt (" + str(prompt) +
           ") was " + str(toc - tic))
+    print(response)
+    elapsed_time = str(toc - tic)
+
+    datablocks.append(transformerLink, "maxlength: " + str(maxlength), elapsed_time, prompt, response)
+
+datablocks.write()
 
 print("Program terminated.")
